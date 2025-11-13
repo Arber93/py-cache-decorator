8678408543f36452f7fc09a33d4eec0e54d6e961
Arber Strazimiri <arberstrazimiri@gmail.com>
2025-11-13 11:45:27 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
+
+cache_results = {}
+
+
+def make_key(args: Any, kwargs: Any) -> tuple:
+    # Convert kwargs to a sorted tuple of (key, value) pairs
+    kwargs_tuple = tuple(sorted(kwargs.items()))
+    return (args, kwargs_tuple)
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        if func not in cache_results:
+            cache_results[func] = {}
+
+        cache_key = make_key(args, kwargs)
+        if cache_key not in cache_results[func]:
+            print("Calculating new result")
+            cache_results[func][cache_key] = func(*args, **kwargs)
+        else:
+            print("Getting from cache")
+
+        return cache_results[func][cache_key]
+    return wrapper
